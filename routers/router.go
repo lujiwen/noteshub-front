91329dd06f5d2180ff072ec46c02ba80e0f2b104
lujiwen <1281476227@qq.com>
2018-07-01 18:05:50 +0800

    before modifcation

--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,10 @@
 package routers
 
 import (
-	"hello/controllers"
 	"github.com/astaxie/beego"
+	"hello/controllers"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
 }
