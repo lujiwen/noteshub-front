cf8f9131181876975e5d60b1a9dfbd0883686b5f
lujiwen <1281476227@qq.com>
2018-07-08 18:30:23 +0800

    added index html combined with react

--- cmd/web.go
+++ cmd/web.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
 var Web = cli.Command{
@@ -33,6 +34,9 @@ func runWeb(c *cli.Context) {
 				"message": "pong",
 			})
 		})
+		api.GET("/jokes", JokeHandler)
+		api.POST("/jokes/like/:jokeID", LikeJoke)
+
 	}
 
 	// Start and run the server
@@ -44,4 +48,48 @@ func userHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"message": "LikeJoke handler not implemented yet",
 	})
+}
+
+
+type Joke struct {
+	ID     int     `json:"id" binding:"required"`
+	Likes  int     `json:"likes"`
+	Joke   string  `json:"joke" binding:"required"`
+}
+
+// We'll create a list of jokes
+var jokes = []Joke{
+	{1, 0, "Did you hear about the restaurant on the moon? Great food, no atmosphere."},
+	{2, 0, "What do you call a fake noodle? An Impasta."},
+	{3, 0, "How many apples grow on a tree? All of them."},
+	{4, 0, "Want to hear a joke about paper? Nevermind it's tearable."},
+	{5, 0, "I just watched a program about beavers. It was the best dam program I've ever seen."},
+	{6, 0, "Why did the coffee file a police report? It got mugged."},
+	{7, 0, "How does a penguin build it's house? Igloos it together."},
+}
+
+// JokeHandler retrieves a list of available jokes
+func JokeHandler(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+	c.JSON(http.StatusOK, jokes)
+}
+
+// LikeJoke increments the likes of a particular joke Item
+func LikeJoke(c *gin.Context) {
+	// confirm Joke ID sent is valid
+	// remember to import the `strconv` package
+	if jokeid, err := strconv.Atoi(c.Param("jokeID")); err == nil {
+		// find joke, and increment likes
+		for i := 0; i < len(jokes); i++ {
+			if jokes[i].ID == jokeid {
+				jokes[i].Likes += 1
+			}
+		}
+
+		// return a pointer to the updated jokes list
+		c.JSON(http.StatusOK, &jokes)
+	} else {
+		// Joke ID is invalid
+		c.AbortWithStatus(http.StatusNotFound)
+	}
 }
