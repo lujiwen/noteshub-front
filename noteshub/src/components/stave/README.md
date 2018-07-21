adf44232665c2b79cd7aaa53b83aa625d999a03a
lujiwen <1281476227@qq.com>
2018-07-21 21:41:24 +0800

    react based vexflow is introduced perfectly

---
+++ noteshub/src/components/stave/README.md
@@ -0,0 +1,17 @@
+# the practice of wrapper vex into a react component
+
+## reference 
+
+https://gist.github.com/wchargin/96f2550531b67c379b3e
+
+## how to run it 
+
+npx webpack 
+
+## how to configure npm registries
+
+nrm 
+
+## how to update local packages
+
+npm update  
---
+++ noteshub/src/components/stave/index.jsx
@@ -0,0 +1,21 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import Notes from './Vex';
+
+import Vex from 'vexflow';
+const {Accidental, StaveNote} = Vex.Flow;
+
+const chord1 = [new StaveNote({
+    keys: ["c/0", "e/4", "g#/8"],
+    duration: "w",
+}).addAccidental(0, new Accidental("bb")).addAccidental(2, new Accidental("#"))];
+const chord2 = [new StaveNote({
+    keys: ["d/4", "f#/4", "a#/4"],
+    duration: "w",
+}).addAccidental(1, new Accidental("#")).addAccidental(2, new Accidental("#"))];
+
+ReactDOM.render(
+    <div>
+        <Notes chord={chord1} />
+        <Notes chord={chord2} />
+    </div>, document.getElementById("app"));
