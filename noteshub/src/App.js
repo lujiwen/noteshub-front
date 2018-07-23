959955c3134106d10ed703549e4b4fcfbb5ff70c
lujiwen <1281476227@qq.com>
2018-07-23 23:59:18 +0800

    encapsulate component finished

--- noteshub/src/App.js
+++ noteshub/src/App.js
@@ -5,11 +5,24 @@ import './components/Vex/Stave'
 import LayoutFooter from "./components/Layout/LayoutFooter";
 import Stave from "./components/Vex/Stave";
 
+import Vex from 'vexflow';
+const {Accidental, StaveNote} = Vex.Flow;
+
 class App extends Component {
   render() {
-    return (
+      let chord1 = [new StaveNote({
+          keys: ["c/0", "e/4", "g#/8"],
+          duration: "w",
+      }).addAccidental(0, new Accidental("bb")).addAccidental(2, new Accidental("#"))];
+
+      let chord2 = [new StaveNote({
+          keys: ["d/4", "f#/4", "a#/4"],
+          duration: "w",
+      }).addAccidental(1, new Accidental("#")).addAccidental(2, new Accidental("#"))];
+
+      return (
       <div className="App">
-          <Stave/>
+          <Stave chord={[chord1,chord2]}/>
           <LayoutFooter/>
       </div>
     );
--- noteshub/src/components/Vex/Stave.jsx
+++ noteshub/src/components/Vex/Stave.jsx
@@ -1,40 +1,18 @@
-import ReactDOM from 'react-dom';
 import React from 'react';
 import Notes from './Vex';
-import Vex from 'vexflow';
-const {Accidental, StaveNote} = Vex.Flow;
-
 
 class Stave extends React.Component {
-    chord1 = [new StaveNote({
-        keys: ["c/0", "e/4", "g#/8"],
-        duration: "w",
-    }).addAccidental(0, new Accidental("bb")).addAccidental(2, new Accidental("#"))];
-     chord2 = [new StaveNote({
-        keys: ["d/4", "f#/4", "a#/4"],
-        duration: "w",
-    }).addAccidental(1, new Accidental("#")).addAccidental(2, new Accidental("#"))];
 
     constructor(props) {
         super(props);
-        // Don't call this.setState() here!
-        // this.state = { counter: 0 };
-        // this.handleClick = this.handleClick.bind(this);
-
-        // this.props.chord1 = [new StaveNote({
-        //     keys: ["c/0", "e/4", "g#/8"],
-        //     duration: "w",
-        // }).addAccidental(0, new Accidental("bb")).addAccidental(2, new Accidental("#"))];
-        // this.props.chord2 = [new StaveNote({
-        //     keys: ["d/4", "f#/4", "a#/4"],
-        //     duration: "w",
-        // }).addAccidental(1, new Accidental("#")).addAccidental(2, new Accidental("#"))];
     }
 
     render() {
+        const { chord} = this.props
         return <div>
-            <Notes chord={this.chord1} />
-            <Notes chord={this.chord2} />
+            <p>{this.props.name}</p>
+            <Notes chord={chord[0]} />
+            <Notes chord={chord[1]} />
         </div>
     }
 }
