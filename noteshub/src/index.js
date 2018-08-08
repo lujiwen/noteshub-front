bdb8bff8e4b9b9c9de220551312ed75f80efb8ae
lujiwen <1281476227@qq.com>
2018-08-08 21:28:03 +0800

    added token

--- noteshub/src/index.js
+++ noteshub/src/index.js
@@ -7,8 +7,12 @@ import registerServiceWorker from './registerServiceWorker';
 import configureStore from './store/configureStore'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
+import {AUTH_SIGNIN} from "./actions/UserAction";
+import { reducer as formReducer } from 'redux-form';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+// import authReducer from "./reducers/authReducer";
 
-const store = configureStore();
+// const store = configureStore();
 const networkInterface = createNetworkInterface({ uri: 'http://localhost:4000/graphql' });
 
 const token = localStorage.getItem('token');
@@ -28,6 +32,26 @@ networkInterface.use([{
 const client = new ApolloClient({
   networkInterface
 });
+
+const store = createStore(
+    combineReducers({
+      apollo: client.reducer(),
+      form: formReducer,
+      // auth: authReducer,
+    }),
+    {}, // initial state
+    compose(
+        applyMiddleware(client.middleware()),
+        // If you are using the devToolsExtension, you can add it here also
+        window.devToolsExtension ? window.devToolsExtension() : f => f,
+    )
+);
+
+if (token) {
+  // We need to update application state if the token exists
+  store.dispatch({ type: AUTH_SIGNIN });
+}
+
 ReactDOM.render(
     <Provider store={store}>
     <App />
