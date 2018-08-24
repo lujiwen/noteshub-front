b095e4f5ea0ef02d1347cee228c92cc2fc2fac38
lujiwen <1281476227@qq.com>
2018-08-24 23:58:20 +0800

    added unit test to test function UpdateMxml and TranslateNoteType

--- models/mxml_parser_test.go
+++ models/mxml_parser_test.go
@@ -7,5 +7,39 @@ import (
 
 
 func TestParseMxml(t *testing.T) {
-	fmt.Print(ParseMxml("./musicxml/sample-chord.xml"))
+	fmt.Print(ParseMxmlFromString("./musicxml/sample-chord.xml"))
+}
+
+func TestUpdateMxml(t *testing.T)  {
+	note := Note{}
+	note.Type = "eight"
+	notes := []Note {note}
+	measure := Measure{}
+	measure.Notes = notes
+	measures := []Measure {measure}
+
+	part := Part{}
+	part.Measures = measures
+	parts := []Part{part}
+
+	sheet := MXLDoc{}
+	sheet.Parts = parts
+	newSheet := sheet.UpdateMxml()
+
+	noteType := newSheet.Parts[0].Measures[0].Notes[0].Type
+
+
+	if noteType != "8" {
+		t.Error("sheet parsing worng, actually it is:", noteType)
+	}
+}
+
+func TestTranslateNoteType(t *testing.T) {
+	note := Note{}
+	note.Type = "eight"
+	note.TranslateNoteType()
+
+	if note.Type != "8" {
+		t.Error("note translating worng, actually it is:", note.Type)
+	}
 }
