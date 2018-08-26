cd3b926fbc146a6507b4cb967f92d7ad0609bb64
lujiwen <1281476227@qq.com>
2018-08-26 11:14:11 +0800

    draw notes on stave

--- noteshub/src/components/Notes.js
+++ noteshub/src/components/Notes.js
@@ -2,16 +2,16 @@ import Vex from 'vexflow';
 import React, {Component} from 'react';
 
 const {
-    Accidental,
-    Formatter,
-    Stave,
-    StaveNote,
-    Renderer,
-    StaveConnector,
-    Voice,
-    RESOLUTION,
-    Tuplet,
-    Beam
+  Accidental,
+  Formatter,
+  Stave,
+  StaveNote,
+  Renderer,
+  StaveConnector,
+  Voice,
+  RESOLUTION,
+  Tuplet,
+  Beam
 } = Vex.Flow;
 
 const SCHEME_WIDTH = 350;
@@ -46,13 +46,13 @@ export default class Notes extends Component {
     this.rowLastBars = [];
 
 
-     this.signature = 'C';
+    this.signature = 'C';
   }
 
-    render() {
-        return <div ref="outer" style={{}}>
-        </div>;
-    }
+  render() {
+    return <div ref="outer" style={{}}>
+    </div>;
+  }
 
   buildNotesVoice(measure, partId, measureId) {
 
@@ -64,8 +64,30 @@ export default class Notes extends Component {
 
     const vexNotes = measure.note.filter(note => note.pitch.step !== "").map(function (note, noteId) {
       let keys = [note.pitch.step + "/" + note.pitch.octave]
-      // let duration = note.duration.toString()
       let duration = "2"
+      // let type = 'r'
+
+      // const { keys, dur: duration, grace, ...options } = note;
+
+      // const noteKeysAccidentals = [];
+      //
+      // const noteTies = [];
+      //
+      // let graceNotes = [];
+
+      // const transposedKeys = keys.map(function ({ ties, ...key }, keyIndex) {
+      //
+      //   const { trStep, trAccidental, trOctave, trPitch: pitch } = this.transposer.transpose(key, duration);
+      //
+      //   noteKeysAccidentals[keyIndex] = trAccidental;
+      //
+      //   if (ties) {
+      //     noteTies[keyIndex] = { pitch, ties };
+      //   }
+      //
+      //   return `${trStep}${trAccidental || ''}/${trOctave}`
+      //
+      // }.bind(this))
 
       const staveNote = new StaveNote({ keys: keys, duration});
 
@@ -74,6 +96,26 @@ export default class Notes extends Component {
       return staveNote;
     }.bind(this));
 
+
+
+    //
+    // if (measure.beams) {
+    //   measure.beams.forEach(beam => {
+    //     const { from, to } = beam;
+    //     this.beams.push(new Beam(vexNotes.slice(from, to)));
+    //   })
+    // } else {
+    //   const autoBeams = Beam.generateBeams(vexNotes);
+    //   this.beams.push(...autoBeams);
+    // }
+
+    // if (measure.tuplets) {
+    //   voice.tuplets.forEach(tuplet => {
+    //     const { from, to, ...options } = tuplet;
+    //     this.tuplets.push(new Tuplet(vexNotes.slice(from, to), options));
+    //   })
+    // }
+
     vexVoice.addTickables(vexNotes);
     return vexVoice
   }
@@ -81,219 +123,179 @@ export default class Notes extends Component {
 
   componentDidMount() {
 
-      const svgContainer = document.createElement('div');
-      const renderer = new Renderer(svgContainer, Renderer.Backends.SVG);
-      const ctx = renderer.getContext();
 
-      const sheet = this.props.sheet;
-      console.log(sheet)
+    const svgContainer = document.createElement('div');
+    const renderer = new Renderer(svgContainer, Renderer.Backends.SVG);
+    const ctx = renderer.getContext();
 
-      const trebleStave = new Stave(0, 0, 800);  // x, y, width
-      const bassStave = new Stave(0, SPACE_BETWEEN_STAVES, 800);  // x, y, width
-      let key = []
-      var duration
-      let partCount = sheet.part.length
-      console.log("total part number: " + partCount)
-      let currentWidth = 0;
+    const sheet = this.props.sheet;
+    console.log(sheet)
 
-      let measureCount = sheet.part[0].measure.length
-      console.log("total measures number: " + measureCount)
-      let firstRow = true;
+    const trebleStave = new Stave(0, 0, 800);  // x, y, width
+    const bassStave = new Stave(0, SPACE_BETWEEN_STAVES, 800);  // x, y, width
+    let key = []
+    var duration
+    let partCount = sheet.part.length
+    console.log("total part number: " + partCount)
 
-      let rowsCounter = 0;
-      // let currentWidth = 0;
-      let currentRowMeasures = [];
-      const widthArray = [];
+    let measureCount = sheet.part[0].measure.length
+    console.log("total measures number: " + measureCount)
 
-      sheet.part.forEach( (part, partId) => {
-        part.measure.forEach((measure, measureId) => {
-            let isFirstMeasure = measureId === 0
-            let isLastMeasure = measureId === (measureCount - 1)
-            let voices = [this.buildNotesVoice(measure, partId, measureId)]
-            const formatter = new Formatter();
-            formatter.joinVoices(voices)
+    // for(let i in measure.note) {
+    //   let note = measure.note[i];
+    //   let pitch = note.pitch;
+    //   if(pitch.step != "" && pitch.step != null){
+    //     key.push(pitch.Step + "/"+pitch.octave);
+    //     duration = note.duration
+    //   }
+    // }
+    //
+    //
+    sheet.part.forEach( (part, partId) => {
+      part.measure.forEach((measure, measureId) => {
+        let isFirstMeasure = measureId === 0
+        let isLastMeasure = measureId === (measureCount - 1)
+        let voices = [this.buildNotesVoice(measure, partId, measureId)]
+        const formatter = new Formatter();
+        formatter.joinVoices(voices)
 
-            const stave = new Stave(0,0)
-            var startX = this.unifyNotesStartX(stave, stave);
-
-            stave.setNoteStartX(startX)
-            stave.setX(0)
-            stave.setY(0)
-            let minTotalWidth = Math.ceil(Math.max(formatter.preCalculateMinTotalWidth(voices), BAR_MIN_WIDTH) * COEFFICIENT);
-
-          const measureWidth = minTotalWidth + (startX - 0) + FIRST_NOTE_SPACE + LAST_NOTE_SPACE;
-
-
-          if (currentWidth + measureWidth < this.sheetWidth) {
-            currentWidth += measureWidth;
-
-            currentRowMeasures.push({
-              measure,
-              measureWidth: measureWidth,
-              minTotalWidth,
-              stave,
-              formatter,
-              voices,
-              isLastMeasure,
-              measureId: `${partId}-${measureId}`
-            })
-
-            widthArray.push(measureWidth)
-            } else {
-              // new stave row
-              // draw current row and begin new row
-              const widthRest = this.sheetWidth - currentWidth;
-              // this.drawStaveRow(currentRowMeasures, widthArray, rowsCounter, widthRest);
-              //newRow = true;
-
-              stave.addClef("treble").addTimeSignature("4/4").addKeySignature(this.signature);
-
-              const startX = Math.max(stave.getNoteStartX(), stave.getNoteStartX());
-              stave.setNoteStartX(startX);
-              // bStave.setNoteStartX(startX);
-
-              const measureWidth = minTotalWidth + (startX - 0) + FIRST_NOTE_SPACE + LAST_NOTE_SPACE;
-
-              rowsCounter++;
-              currentWidth = measureWidth;
-              currentRowMeasures.length = 0;
-              widthArray.length = 0;
-              widthArray.push(measureWidth);
-              currentRowMeasures.push({
-                measure,
-                measureWidth: measureWidth,
-                minTotalWidth,
-                stave,
-                formatter,
-                voices,
-                isLastMeasure: isLastMeasure,
-                barId: `${partId}-${measureId}`
-              });
-
-            }
-
-            // Add a clef and time signature.
-            if (firstRow) {
-              stave.addClef("treble").addTimeSignature("4/4").addKeySignature(this.signature);
-              firstRow = false;
-            }
-
-
-            stave.setWidth(400)
-            formatter.format(voices,0)
-
-            stave.setContext(ctx).draw()
-            voices.forEach(function (v) { v.draw(ctx, stave); }.bind(this));
-        })
+        const stave = new Stave(0,0)
+        stave.addClef("treble").addTimeSignature("4/4").addKeySignature(this.signature);
+        stave.setNoteStartX(startX)
+        stave.setX(0)
+        stave.setY(0)
+        stave.setWidth(400)
+        formatter.format(voices,0)
+        stave.setContext(ctx).draw()
+        voices.forEach(function (v) { v.draw(ctx, stave); }.bind(this));
       })
-
-      ctx.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
-
+    })
 
 
-      const sectionsLength = 10;
-      //
-      // trebleStave.addClef("treble").addTimeSignature("4/4").addKeySignature(this.signature).setContext(ctx).draw();
-      // bassStave.addClef("bass").addTimeSignature("4/4").addKeySignature(this.signature).setContext(ctx).draw();
 
 
-      // var barWidth = minTotalWidth + (startX - 0) + FIRST_NOTE_SPACE + LAST_NOTE_SPACE;
 
-      // const lineLeft = new StaveConnector(trebleStave, bassStave).setType(1);
-      // const lineRight = new StaveConnector(trebleStave, bassStave).setType(0);
-      //
-      // lineLeft.setContext(ctx).draw();
-      // lineRight.setContext(ctx).draw();
-
-      // const bb = Formatter.FormatAndDraw(ctx, trebleStave, chord);
-      const svg = svgContainer.childNodes[0];
-      const padding = 10;
-      const half = padding / 2;
-      // svg.style.top = -bb.y + half + Math.max(0, (100 - bb.h) * 2/3) + "px";
-      // svg.style.height = Math.max(100, bb.h);
-      svg.style.top = 10
-      svg.style.height = 1000
-      svg.style.left = PADDING_LEFT;
-      svg.style.width = 100 + "px";
-      svg.style.position = "absolute";
-      svg.style.overflow = "visible";
-      // svgContainer.style.height = Math.max(100, bb.h + padding) + "px";
-      svgContainer.style.width = 100 + "px";
-      svgContainer.style.height = 1000 + "px"
-      svgContainer.style.position = "relative";
-      svgContainer.style.display = "inlineBlock";
-
-        // noinspection JSUnresolvedVariable
-      this.refs.outer.appendChild(svgContainer);
-    }
-
-      unifyNotesStartX(trebleStave, bassStave) {
-      const startX = Math.max(trebleStave.getNoteStartX(), bassStave.getNoteStartX());
-      trebleStave.setNoteStartX(startX);
-      bassStave.setNoteStartX(startX);
-      return startX
-    }
+    //
+    //
+    // let chord = [new StaveNote({
+    //   keys: key,
+    //   duration: duration.toString(),
+    // })];
 
 
-    calculateMeasureWidthRatio(arr, value, precision) {
-      const sum = arr.reduce((sum, value) => sum + value, 0);
 
-      const percentages = arr.map(value => value / sum);
+    ctx.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
 
-      const valueDistribution = percentages.map(percentage => {
-        const factor = Math.pow(10, precision);
-        return Math.round(value * percentage * factor) / factor;
-      });
+    let rowsCounter = 0;
+    let currentWidth = 0;
+    const currentRowBars = [];
+    const widthArray = [];
+    let firstRow = true;
 
-      return valueDistribution;
-    }
+    const sectionsLength = 10;
+    //
+    // trebleStave.addClef("treble").addTimeSignature("4/4").addKeySignature(this.signature).setContext(ctx).draw();
+    // bassStave.addClef("bass").addTimeSignature("4/4").addKeySignature(this.signature).setContext(ctx).draw();
 
-    // draw a single row of stave with full parts(tacks)
-    drawStaveRow(currentRowMeasures, sectionWidthArray, currentRowIndex, widthRest = 0) {
+    var startX = this.unifyNotesStartX(trebleStave, bassStave);
 
-        this.rowFirstBars.push(currentRowMeasures[0].barId);
-        this.rowLastBars.push(currentRowMeasures[currentRowMeasures.length - 1].barId);
+    // var barWidth = minTotalWidth + (startX - 0) + FIRST_NOTE_SPACE + LAST_NOTE_SPACE;
 
-        let barOffset = PADDING_LEFT;
+    // const lineLeft = new StaveConnector(trebleStave, bassStave).setType(1);
+    // const lineRight = new StaveConnector(trebleStave, bassStave).setType(0);
+    //
+    // lineLeft.setContext(ctx).draw();
+    // lineRight.setContext(ctx).draw();
 
-        const widthRestArray = this.calculateMeasureWidthRatio(sectionWidthArray, widthRest, 0);
+    // const bb = Formatter.FormatAndDraw(ctx, trebleStave, chord);
+    const svg = svgContainer.childNodes[0];
+    const padding = 10;
+    const half = padding / 2;
+    // svg.style.top = -bb.y + half + Math.max(0, (100 - bb.h) * 2/3) + "px";
+    // svg.style.height = Math.max(100, bb.h);
+    svg.style.top = 10
+    svg.style.height = 1000
+    svg.style.left = PADDING_LEFT;
+    svg.style.width = 100 + "px";
+    svg.style.position = "absolute";
+    svg.style.overflow = "visible";
+    // svgContainer.style.height = Math.max(100, bb.h + padding) + "px";
+    svgContainer.style.width = 100 + "px";
+    svgContainer.style.height = 1000 + "px"
+    svgContainer.style.position = "relative";
+    svgContainer.style.display = "inlineBlock";
 
-        currentRowMeasures.forEach((b, index) => {
+    // noinspection JSUnresolvedVariable
+    this.refs.outer.appendChild(svgContainer);
+  }
 
-          const barWidth = b.barWidth + (widthRest !== 0 ? widthRestArray[index] : 0)
+  unifyNotesStartX(trebleStave, bassStave) {
+    const startX = Math.max(trebleStave.getNoteStartX(), bassStave.getNoteStartX());
+    trebleStave.setNoteStartX(startX);
+    bassStave.setNoteStartX(startX);
+    return startX
+  }
 
-          const trebleStave = b.tStave;
-          const bassStave = b.bStave;
 
-          trebleStave.setX(barOffset);
-          bassStave.setX(barOffset);
-          trebleStave.setY(PADDING_TOP + currentRowIndex * SPACE_BETWEEN_GRAND_STAVES);
-          bassStave.setY(PADDING_TOP + currentRowIndex * SPACE_BETWEEN_GRAND_STAVES + SPACE_BETWEEN_STAVES);
+  calculateMeasureWidthRatio(arr, value, precision) {
+    const sum = arr.reduce((sum, value) => sum + value, 0);
 
-          trebleStave.setWidth(barWidth);
-          bassStave.setWidth(barWidth);
+    const percentages = arr.map(value => value / sum);
 
-          barOffset += barWidth;
+    const valueDistribution = percentages.map(percentage => {
+      const factor = Math.pow(10, precision);
+      return Math.round(value * percentage * factor) / factor;
+    });
 
-          if (b.text) {
-            // trebleStave.setText(b.text[0], VF.Modifier.Position.ABOVE, { shift_y: 0, justification: VF.TextNote.Justification.LEFT });
-            trebleStave.setSection(b.text[0], 0);
-          }
+    return valueDistribution;
+  }
 
-          const lineLeft = new StaveConnector(trebleStave, bassStave).setType(1);
-          const lineRight = new StaveConnector(trebleStave, bassStave).setType(b.isLastBar ? 6 : 0);
+  // draw a single row of stave with full parts(tacks)
+  drawStaveRow(currentRowMeasures, sectionWidthArray, currentRowIndex, widthRest = 0) {
 
-          trebleStave.setContext(this.context).draw();
-          bassStave.setContext(this.context).draw();
+    this.rowFirstBars.push(currentRowMeasures[0].barId);
+    this.rowLastBars.push(currentRowMeasures[currentRowMeasures.length - 1].barId);
 
-          lineLeft.setContext(this.context).draw();
-          lineRight.setContext(this.context).draw();
+    let barOffset = PADDING_LEFT;
 
-          b.formatter.format(b.trebleStaveVoices.concat(b.bassStaveVoices), b.minTotalWidth + (widthRest !== 0 ? widthRestArray[index] : 0));
+    const widthRestArray = this.calculateMeasureWidthRatio(sectionWidthArray, widthRest, 0);
 
-          // Render voices
-          b.trebleStaveVoices.forEach(function (v) { v.draw(this.context, trebleStave); }.bind(this));
-          b.bassStaveVoices.forEach(function (v) { v.draw(this.context, bassStave); }.bind(this));
+    currentRowMeasures.forEach((b, index) => {
+
+      const barWidth = b.barWidth + (widthRest !== 0 ? widthRestArray[index] : 0)
+
+      const trebleStave = b.tStave;
+      const bassStave = b.bStave;
+
+      trebleStave.setX(barOffset);
+      bassStave.setX(barOffset);
+      trebleStave.setY(PADDING_TOP + currentRowIndex * SPACE_BETWEEN_GRAND_STAVES);
+      bassStave.setY(PADDING_TOP + currentRowIndex * SPACE_BETWEEN_GRAND_STAVES + SPACE_BETWEEN_STAVES);
+
+      trebleStave.setWidth(barWidth);
+      bassStave.setWidth(barWidth);
+
+      barOffset += barWidth;
+
+      if (b.text) {
+        // trebleStave.setText(b.text[0], VF.Modifier.Position.ABOVE, { shift_y: 0, justification: VF.TextNote.Justification.LEFT });
+        trebleStave.setSection(b.text[0], 0);
+      }
+
+      const lineLeft = new StaveConnector(trebleStave, bassStave).setType(1);
+      const lineRight = new StaveConnector(trebleStave, bassStave).setType(b.isLastBar ? 6 : 0);
+
+      trebleStave.setContext(this.context).draw();
+      bassStave.setContext(this.context).draw();
+
+      lineLeft.setContext(this.context).draw();
+      lineRight.setContext(this.context).draw();
+
+      b.formatter.format(b.trebleStaveVoices.concat(b.bassStaveVoices), b.minTotalWidth + (widthRest !== 0 ? widthRestArray[index] : 0));
+
+      // Render voices
+      b.trebleStaveVoices.forEach(function (v) { v.draw(this.context, trebleStave); }.bind(this));
+      b.bassStaveVoices.forEach(function (v) { v.draw(this.context, bassStave); }.bind(this));
 
     });
   }
