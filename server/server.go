18c30e9078dc807f8a37a38a50bd38dcb8f5e1b9
lujiwen <1281476227@qq.com>
2018-09-11 22:31:54 +0800

    successfully response client with right login result

--- server/server.go
+++ server/server.go
@@ -1,20 +1,26 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
+	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
 	log "gopkg.in/clog.v1"
-	"time"
-	"strconv"
-	"fmt"
 	"io/ioutil"
 	"mo/models"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
 )
 
 
 func main() {
 	// Set the router as the default one shipped with Gin
 	router := gin.Default()
+
+	store := sessions.NewCookieStore([]byte("secret"))
+	router.Use(sessions.Sessions("mysession", store))
+
 
 	// connect to db
 	//initializeDB()
@@ -32,6 +38,8 @@ func main() {
 		api.GET("/sheet/:sheetId", GetSheet)
 
 		api.POST("/upload", Upload)
+
+		api.POST("/login", login)
 	}
 
 	// Start and run the server
@@ -89,4 +97,39 @@ type Sheet struct {
 	UserId           string `json:"userId" binding:"required"`
 	CreateTime       time.Time `json:"createTime" binding:"required"`
 	LastModifiedTime time.Time `json:"lastModifiedTime" binding:"required"`
+}
+
+
+func login(c *gin.Context) {
+	session := sessions.Default(c)
+	username := c.PostForm("username")
+	password := c.PostForm("password")
+
+	if strings.Trim(username, " ") == "" || strings.Trim(password, " ") == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Parameters can't be empty"})
+		return
+	}
+	if username == "hello" && password == "itsme" {
+		session.Set("user", username) //In real world usage you'd set this to the users ID
+		err := session.Save()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate session token"})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"message": "Successfully authenticated user"})
+		}
+	} else {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Authentication failed"})
+	}
+}
+
+func logout(c *gin.Context) {
+	session := sessions.Default(c)
+	user := session.Get("user")
+	if user == nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid session token"})
+	} else {
+		session.Delete("user")
+		session.Save()
+		c.JSON(http.StatusOK, gin.H{"message": "Successfully logged out"})
+	}
 }
