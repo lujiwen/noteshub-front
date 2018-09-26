fe2b5eb08afa5195b1550ca988264da14aadfc4a
lujiwen <1281476227@qq.com>
2018-09-26 22:59:08 +0800

    update left draw

--- noteshub/src/components/LeftDrawer.js
+++ noteshub/src/components/LeftDrawer.js
@@ -1,20 +1,30 @@
 import React, { Component} from "react"
-import { Drawer, Button } from 'antd';
+import { Drawer, Button, Menu, Icon } from 'antd';
 import { connect } from 'react-redux';
 import {toggleLeftDrawer} from "../actions/NavigationAction";
 
 const LeftDrawer = ({visible, onClose}) => (
     <div>
       <Drawer
-          title="Basic Drawer"
+          title="芸芸曲谱"
           placement="left"
           closable={true}
           onClose={onClose}
           visible={visible}
       >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+
+        <Menu theme="light" mode="inline" defaultSelectedKeys={['1']}>
+          <Menu.Item key="personal_page">
+            <Icon type="user" />
+            <span>个人主页</span>
+          </Menu.Item>
+
+          <Menu.Item key="upload_sheet">
+            <Icon type="upload" />
+            <span>上传曲谱</span>
+          </Menu.Item>
+        </Menu>
+
       </Drawer>
     </div>
 )
