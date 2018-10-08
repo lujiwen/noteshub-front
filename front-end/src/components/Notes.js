1cf2fd8711819a1d067176be073cb8f9d9ea7f10
lujiwen <1281476227@qq.com>
2018-10-08 23:52:27 +0800

    fix bug in parsing treble stave

--- front-end/src/components/Notes.js
+++ front-end/src/components/Notes.js
@@ -85,10 +85,12 @@ export default class Notes extends Component {
         duration = "qr"
         keys = ["B/4"]  // temporary solution: in the middle of treble
       }
-      const staveNote = new StaveNote({keys: keys, duration: duration, clef: "bass"});
 
-
-      for (let i = 0; i < keys.length; i++) {
+      let staveNote
+      if (this.staveType == StaveType.TREBLE || this.staveType == StaveType.BASS) {
+         staveNote = new StaveNote({keys: keys, duration: duration, clef: this.staveType});
+      }
+        for (let i = 0; i < keys.length; i++) {
         let key = keys[i]
         if (key.includes("#")) {
           staveNote.addAccidental(i, new Accidental("#"))
