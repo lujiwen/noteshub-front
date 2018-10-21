bf63c970c3d25cae982231dfad2de38a6dd61d0f
lujiwen <1281476227@qq.com>
2018-10-21 10:25:41 +0800

    refactored LeftDraw by using class declaration instead of function

--- front-end/src/components/LeftDrawer.js
+++ front-end/src/components/LeftDrawer.js
@@ -2,33 +2,69 @@ import React from "react"
 import {Drawer, Icon, Menu} from 'antd';
 import {connect} from 'react-redux';
 import {toggleLeftDrawer} from "../actions/NavigationAction";
+//
+// const LeftDrawer = ({visible, onClose}) => (
+//     <div>
+//       <Drawer
+//           title="芸芸曲谱"
+//           placement="left"
+//           closable={true}
+//           onClose={onClose}
+//           visible={visible}
+//       >
+//
+//         <Menu theme="light" mode="inline" defaultSelectedKeys={['1']}>
+//           <Menu.Item key="personal_page">
+//             <Icon type="user" />
+//             <span>个人主页</span>
+//           </Menu.Item>
+//
+//           <Menu.Item key="upload_sheet">
+//             <Icon type="upload" />
+//             <span onClick={redirectToUpload}>上传曲谱</span>
+//           </Menu.Item>
+//         </Menu>
+//
+//       </Drawer>
+//     </div>
+// )
+//
+// const redirectToUpload= () => {
+//   // this.context.router.history.push('upload')
+// }
 
-const LeftDrawer = ({visible, onClose}) => (
-    <div>
-      <Drawer
-          title="芸芸曲谱"
-          placement="left"
-          closable={true}
-          onClose={onClose}
-          visible={visible}
-      >
+class LeftDrawer extends React.Component {
 
-        <Menu theme="light" mode="inline" defaultSelectedKeys={['1']}>
-          <Menu.Item key="personal_page">
-            <Icon type="user" />
-            <span>个人主页</span>
-          </Menu.Item>
+    render() {
+     const {visible, onClose} = this.props
 
-          <Menu.Item key="upload_sheet">
-            <Icon type="upload" />
-            <span>上传曲谱</span>
-          </Menu.Item>
-        </Menu>
+      return (
+          <div>
+            <Drawer
+                title="芸芸曲谱"
+                placement="left"
+                closable={true}
+                onClose={onClose}
+                visible={visible}
+            >
 
-      </Drawer>
-    </div>
-)
+              <Menu theme="light" mode="inline" defaultSelectedKeys={['1']}>
+                <Menu.Item key="personal_page">
+                  <Icon type="user" />
+                  <span>个人主页</span>
+                </Menu.Item>
 
+                <Menu.Item key="upload_sheet">
+                  <Icon type="upload" />
+                  <span>上传曲谱</span>
+                </Menu.Item>
+              </Menu>
+
+            </Drawer>
+          </div>
+      )
+    }
+}
 
 
 function mapStateToProps(state) {
