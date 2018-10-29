552b40d822cd8e8c4fed6b86175dab5e10667268
lujiwen <1281476227@qq.com>
2018-10-30 00:21:41 +0800

    added post request to upload sheet and info

--- front-end/src/actions/SheetActions.js
+++ front-end/src/actions/SheetActions.js
@@ -1,5 +1,37 @@
+import {AxiosInstance as axios} from "axios";
+
 const startToUploadSheet = (dispatch, values) => {
   console.log("start to upload sheets :" + values)
+  let data = JSON.stringify({
+    song: values.song,
+    composer: values.composer,
+    lyricist: values.lyricist,
+    arranger: values.arranger,
+    scorer: values.scorer,
+    sheetPath: values.sheetPath,
+    sheetType: values.sheetType,
+    sheets : values.sheets
+  })
+
+  axios.post("http://127.0.0.1:8080/uploadSheetAndInfo", data, {
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+    }
+  })
+      .then(function (response) {
+        console.log("success:" + response)
+        dispatch({
+          type:'UPLOAD_SUCCEED',
+          payload: response.data
+        });
+      })
+      .catch((err) => {
+        console.log("failed :" + err)
+        dispatch({
+          type:'UPLOAD_FAILED',
+          payload: err
+        });
+      })
 }
 
 export {startToUploadSheet}
